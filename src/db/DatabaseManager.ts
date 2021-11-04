09fd1ee4f6561a1a31104f71e617b9140fed84c4
Jadson Luan <jadson.silva@splab.ufcg.edu.br>
2021-11-04 19:38:11 -0300

    Update service related classes

--- src/db/DatabaseManager.ts
+++ src/db/DatabaseManager.ts
@@ -1,9 +1,10 @@
 import { Connection, getConnection } from "typeorm";
 import db from ".";
-import { NODE_ENV } from "../config/environment";
+import { NODE_ENV, DB_ENV } from "../config/environment";
 import Movie from "../entities/Movie";
+import fs from 'fs'
 
-class DatabaseManager {
+export default abstract class DatabaseManager {
   connection: Connection | undefined
 
   constructor(conn?: Connection) {
@@ -31,4 +32,55 @@ class DatabaseManager {
   }
 }
 
-export default new DatabaseManager()
+export class EntityBaseServiceManager extends DatabaseManager {
+  getMigrationFileNames() {
+    const list: string[] = [];
+    fs.readdirSync(DB_ENV.MIGRATIONS_FOLDER, {}).forEach((file) =>
+      list.push(file.toString())
+    );
+    return list;
+  }
+
+  private async getMigration(
+    name: string
+  ): Promise<{ [key: string]: any } | undefined> {
+    if (/.*(.ts|.js)$/.test(name)) {
+      return import(`${DB_ENV.MIGRATIONS_FOLDER}/${name}`);
+    } else {
+      return undefined;
+    }
+  }
+
+  /**
+   * Custom function for run migrations
+   * Experimental
+   * WIP
+   */
+  async runMigrations() {
+    const files = this.getMigrationFileNames();
+    const queryRunner = this.getDBManager().connection.createQueryRunner();
+    for (const name of files) {
+      const migrationsObject = await this.getMigration(name);
+      if (migrationsObject) {
+        const keys = Object.keys(migrationsObject);
+        for (const migrationKey of keys) {
+          await new migrationsObject[migrationKey]()?.up(queryRunner);
+        }
+      }
+    }
+  }
+
+  async flushDatabase() {
+    const connection = await this.getDBManager().connection;
+
+    await connection
+      .createQueryRunner()
+      .dropSchema(DB_ENV.DATABASE_SCHEMA, true, true);
+
+    await connection
+      .createQueryRunner()
+      .createSchema(DB_ENV.DATABASE_SCHEMA, true);
+
+    await connection.runMigrations();
+  }
+}
--- src/services/MovieService.ts
+++ src/services/MovieService.ts
@@ -1,51 +1,46 @@
 import DatabaseManager from "../db/DatabaseManager";
 import Movie from "../entities/Movie";
 
-
-async function get(id?: number | string): Promise<Movie> {
-  if (!id) {
-    throw Error("id not provided");
+class MovieService extends DatabaseManager {
+  async get(id?: number | string): Promise<Movie> {
+    if (!id) {
+      throw Error("id not provided");
+    }
+  
+    if (typeof (id) === 'string') id = Number(id)
+  
+    return this.getMovie().findOne(id)
   }
-
-  if (typeof (id) === 'string') id = Number(id)
-
-  return DatabaseManager.getMovie().findOne(id)
-}
-
-async function create(data: Movie): Promise<Movie> {
-  if (!data.name) {
-    throw Error("name not provided");
+  
+  async create(data: Movie): Promise<Movie> {
+    if (!data.name) {
+      throw Error("name not provided");
+    }
+  
+    const createdEntity = this.getMovie().create(data)
+    return this.getMovie().save(createdEntity)
   }
-
-  const createdEntity = DatabaseManager.getMovie().create(data)
-  console.log(createdEntity)
-  return DatabaseManager.getMovie().save(createdEntity)
-}
-
-async function list(limit = 10, page = 1): Promise<Movie[]> {
-  if (limit < 0) limit = 10
-  if (page <= 0) page = 1
-  return DatabaseManager.getMovie().find({ skip: (page - 1) * limit, take: limit });
-}
-
-async function update(id, data: Movie): Promise<boolean> {
-  if (!id) {
-    throw Error("id not provided");
+  
+  async list(limit = 10, page = 1): Promise<Movie[]> {
+    if (limit < 0) limit = 10
+    if (page <= 0) page = 1
+    return this.getMovie().find({ skip: (page - 1) * limit, take: limit });
   }
-
-  if (!data.name) return false;
-
-  try {
-    DatabaseManager.getMovie().save({ id, ...data })
-    return true
-  } catch (error) {
-    return false
+  
+  async update(id, data: Movie): Promise<boolean> {
+    if (!id) {
+      throw Error("id not provided");
+    }
+  
+    if (!data.name) throw Error("name not provided");
+  
+    try {
+      await this.getMovie().save({ id, ...data })
+      return true
+    } catch (error) {
+      return false
+    }
   }
 }
 
-export default {
-  create,
-  get,
-  list,
-  update
-}
+export default new MovieService()
